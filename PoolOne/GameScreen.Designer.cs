01e3ec259a840e3044b267304bf83045430903d6
eliasant579 <eliasant579@SCS-D-02VZ1X>
2019-05-28 09:29:50 -0400

    Some design is done. I MIGHT want to make the secondary screens smaller

--- PoolOne/GameScreen.Designer.cs
+++ PoolOne/GameScreen.Designer.cs
@@ -28,12 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.clothPlaceHolderLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // clothPlaceHolderLabel
+            // 
+            this.clothPlaceHolderLabel.BackColor = System.Drawing.Color.DarkGreen;
+            this.clothPlaceHolderLabel.Location = new System.Drawing.Point(30, 30);
+            this.clothPlaceHolderLabel.Name = "clothPlaceHolderLabel";
+            this.clothPlaceHolderLabel.Size = new System.Drawing.Size(740, 440);
+            this.clothPlaceHolderLabel.TabIndex = 0;
+            this.clothPlaceHolderLabel.Text = "clothPlaceHolderLabel";
+            this.clothPlaceHolderLabel.Visible = false;
             // 
             // GameScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.Controls.Add(this.clothPlaceHolderLabel);
             this.Name = "GameScreen";
             this.Size = new System.Drawing.Size(800, 500);
             this.ResumeLayout(false);
@@ -41,5 +54,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Label clothPlaceHolderLabel;
     }
 }
