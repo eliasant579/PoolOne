0d28b3718d938819f4ce29cca84f490150620c67
eliasant579 <eliasant579@SCS-D-02VZ1X>
2019-06-06 10:06:44 -0400

    Good

--- PoolOne/Screens/GameScreen.cs
+++ PoolOne/Screens/GameScreen.cs
@@ -12,7 +12,7 @@ namespace PoolOne
 {
     public partial class GameScreen : UserControl
     {
-        const int BALL_NUMBER = 2;
+        const int BALL_NUMBER = 12;
 
         const int BORDER_SIZE = 30;
         const float FRICTION_COEFFICIENT = 0.01f;
@@ -173,7 +173,7 @@ namespace PoolOne
                 ballsArray[0].position = new Vector2d(startPositionArray[0].X, startPositionArray[0].Y);
             }
 
-            /*Actual ball 
+            //*Actual ball 
             if (ballsStopped)
             {
                 Vector2d tempVelocity = new Vector2d(velocityInputVector.x, velocityInputVector.y);
@@ -210,7 +210,7 @@ namespace PoolOne
             }
             //*/
 
-            //*
+            /*
             //Testing
             if (downArrowDown)
             {
@@ -275,6 +275,7 @@ namespace PoolOne
                             if (pocketsArray[j].PocketCollsion(ballCentre))
                             {
                                 ballsArray[i].inPocket = true;
+                                ballsArray[i].velocity = new Vector2d(0, 0);
                                 break;
                             }
                         }
@@ -403,6 +404,7 @@ namespace PoolOne
 
             thisScreen.Controls.Add(ms);
             ms.BringToFront();
+            ms.Focus();
         }
 
         public static void RemoveMenuScreen(MenuScreen ms, int n)
